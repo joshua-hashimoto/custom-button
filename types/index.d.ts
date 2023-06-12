7f5d565cea98b2e1c20b9d9d85d96d1a6af1722a
joshua-hashimoto <dropscar041205@gmail.com>
2023-06-13 00:55:57 +0900

    delete: index.d.ts

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Button from "../src/components";
-declare const _Button: React.ComponentType<typeof Button>;
-export { _Button };
-declare const _default: React.ComponentType;
-export default _default;
