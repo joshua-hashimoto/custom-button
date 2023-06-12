fed6269f7e355066761706a59ba1320d7939e044
joshua-hashimoto <dropscar041205@gmail.com>
2023-06-12 23:40:38 +0900

    fix: add dist in git

---
+++ dist/components/Button/Button.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react" />
+export type ButtonProps = JSX.IntrinsicElements["a"] & {
+    title: string;
+    onTap: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+};
+declare const Button: React.FC<ButtonProps>;
+export default Button;
