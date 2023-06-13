59476cf4fcfc4d7fda04b78092091f4c42fe149d
joshua-hashimoto <dropscar041205@gmail.com>
2023-06-13 22:50:03 +0900

    delete: dist

--- dist/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-export type ButtonProps = JSX.IntrinsicElements["a"] & {
-    title: string;
-    onTap: (event: React.MouseEvent<HTMLAnchorElement>) => void;
-};
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- dist/components/Button/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Button from "./Button";
-export default Button;
-export type { ButtonProps } from "./Button";
