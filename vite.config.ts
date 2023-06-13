76e438b5642be799d2e1248c4ab749564096b1b0
joshua-hashimoto <dropscar041205@gmail.com>
2023-06-13 22:51:48 +0900

    fix: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import typescript from "@rollup/plugin-typescript";
 import path from "path";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -14,35 +11,21 @@ export default defineConfig({
     }),
   ],
   build: {
-    manifest: true,
-    minify: true,
-    reportCompressedSize: true,
+    sourcemap: true,
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "custom-button",
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "MyLib",
       formats: ["es", "umd"],
       fileName: (format) => `custom-button.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
         },
       },
-      plugins: [
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        typescript({
-          sourceMap: false,
-          declaration: true,
-          outDir: "dist",
-        }),
-      ],
     },
   },
 });
